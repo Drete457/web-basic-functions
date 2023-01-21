982fbd76c2427e02a5e07c529664e559fea5698d
Filipe Mota <nada.geral@gmail.com>
2023-01-21 15:08:55 +0000

    main page content

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -1,3 +1,50 @@
-const MainPage: React.FC = () => <p>Main Page</p>;
+const MainPage: React.FC = () => (
+    <section>
+        <h1>Web Basic Functions</h1>
+
+        <hr />
+
+        <section>
+            <h1>
+                Description The fundamental functions that a Front - End
+                Developer may require for their projects:
+            </h1>
+            <ul>
+                <li>- 🛠️ Basic Front - End Functions</li>
+                <li>- 🔑 swiss army knife style</li>
+                <li>- 📝 Documentation</li>
+                <li>- 📚 Examples</li>
+            </ul>
+        </section>
+
+        <hr />
+
+        <section>
+            <h2>
+                Cloning the Repository With Git and Node.js installed on your
+                machine, choose location and clone repository, use this
+                commands:
+            </h2>
+            <ol>
+                <li>
+                    git clone
+                    https://github.com/Drete457/web-basic-functions.git
+                </li>
+                <li>cd web-basic-functions</li>
+                <li>yarn install</li>
+                <li>yarn dev - to run application</li>
+                <li>yarn build - to generate the package in the dist folder</li>
+                <li>np - to publish the package</li>
+            </ol>
+        </section>
+
+        <hr />
+
+        <section>
+            <h2>Documentation</h2>
+            <p>- under construction</p>
+        </section>
+    </section>
+);
 
 export default MainPage;
