ba3af4df37b5326580aaa378fe3b799769872daf
Filipe Mota <nada.geral@gmail.com>
2023-01-21 11:46:13 +0000

    body view with routes

--- src/view/body/body.tsx
+++ src/view/body/body.tsx
@@ -1,5 +1,31 @@
-const Body: React.FC = () => {
-    return <main>Body</main>;
-};
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { routing, routesPath } from '@/routing';
+
+const Body: React.FC = () => (
+    <main>
+        <BrowserRouter>
+            <Routes>
+                {routing.map(route => (
+                    <Route
+                        key={route.name}
+                        path={route.path}
+                        element={
+                            <route.component
+                                key={route.name}
+                                type={null}
+                                props=""
+                            />
+                        }
+                    />
+                ))}
+                <Route
+                    key="*"
+                    path="*"
+                    element={<Navigate to={routesPath.mainPage.url} />}
+                />
+            </Routes>
+        </BrowserRouter>
+    </main>
+);
 
 export default Body;
