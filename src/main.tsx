1573c837de6e32780e5d855889c476ce2254200b
Filipe Mota <nada.geral@gmail.com>
2023-01-22 13:20:37 +0000

    header responsive

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import '@/style/reset.css';
+import '@/style/index.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') || document.createElement('div'),
--- src/view/header/header.tsx
+++ src/view/header/header.tsx
@@ -1,30 +1,70 @@
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 import { routesPath } from '@/routing';
 import logo from '@/assets/logo-no-background.png';
 import '@/style/header.css';
 
 const Header: React.FC = () => {
     const links = Object.entries(routesPath);
+    const [showMenu, setShowMenu] = useState<boolean>(false);
 
     return (
         <header className="header-style">
-            <img src={logo} alt="project logo" />
-            <nav>
-                {links.map(link => {
-                    const [key, { url, name }] = link;
+            <Link
+                onClick={() => setShowMenu(false)}
+                to={routesPath.mainPage.url}
+                title={`Go to ${routesPath.mainPage.name}`}
+            >
+                <img src={logo} alt="project logo" className="logo" />
+            </Link>
 
-                    return (
-                        <NavLink
-                            key={key}
-                            to={url}
-                            className={({ isActive }) =>
-                                isActive ? 'active-link' : 'disabled-link'
-                            }
-                        >
-                            {name}
-                        </NavLink>
-                    );
-                })}
+            <nav>
+                <button
+                    className="dropdown-button"
+                    onClick={() => setShowMenu((value: boolean) => !value)}
+                    title="Show navigation menu"
+                    type="button"
+                >
+                    <div
+                        className={`menu-hamburger ${
+                            showMenu ? 'open' : 'close'
+                        }`}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </button>
+
+                <div
+                    className={`${showMenu ? 'dropdown-menu' : 'hide-element'}`}
+                >
+                    <ul className="dropdown-content">
+                        {links.map(link => {
+                            const [key, { url, name }] = link;
+
+                            return (
+                                <li key={key} className="dropdown-content_item">
+                                    <NavLink
+                                        className={({ isActive }) =>
+                                            isActive
+                                                ? 'active-link'
+                                                : 'disabled-link'
+                                        }
+                                        onClick={() => setShowMenu(false)}
+                                        title={`Go to ${name}`}
+                                        to={url}
+                                    >
+                                        {name}
+                                    </NavLink>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
             </nav>
         </header>
     );
