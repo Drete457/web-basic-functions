155c5f8d16486737cef41e76867be57847dc2da1
Filipe Mota <nada.geral@gmail.com>
2023-01-29 14:44:30 +0000

    main page styling

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -1,50 +1,127 @@
-const MainPage: React.FC = () => (
-    <section>
-        <h1>Web Basic Functions</h1>
+import copyIcon from '@/assets/icons/copy.svg';
+import '@/style/main-page.css';
 
-        <hr />
+const MainPage: React.FC = () => {
+    const handleCopyToClipboard = (event): void => {
+        const { target } = event;
 
+        if (
+            target &&
+            target.parentElement &&
+            target.parentElement.parentElement
+        ) {
+            const titleOfTheGrandparent =
+                target.parentElement.parentElement.title;
+
+            navigator.clipboard.writeText(titleOfTheGrandparent);
+        }
+    };
+
+    return (
         <section>
-            <h1>
-                Description The fundamental functions that a Front - End
-                Developer may require for their projects:
-            </h1>
-            <ul>
-                <li>- 🛠️ Basic Front - End Functions</li>
-                <li>- 🔑 swiss army knife style</li>
-                <li>- 📝 Documentation</li>
-                <li>- 📚 Examples</li>
-            </ul>
+            <h1>Web Basic Functions</h1>
+
+            <hr />
+
+            <section>
+                <h2>
+                    Description The fundamental functions that a Front - End
+                    Developer may require for their projects:
+                </h2>
+
+                <ul className="description-list">
+                    <li>- 🛠️ Basic Front - End Functions</li>
+                    <li>- 🔑 swiss army knife functions</li>
+                    <li>- 📝 Documentation</li>
+                    <li>- 📚 Examples</li>
+                </ul>
+            </section>
+
+            <hr />
+
+            <section>
+                <h2>
+                    Cloning the Repository With Git and Node.js installed on
+                    your machine, choose location and clone repository, use this
+                    commands:
+                </h2>
+
+                <ol className="starting-list">
+                    <li
+                        title="git clone
+                        https://github.com/Drete457/web-basic-functions.git"
+                    >
+                        git clone
+                        https://github.com/Drete457/web-basic-functions.git
+                        <button type="button" onClick={handleCopyToClipboard}>
+                            <img
+                                src={copyIcon}
+                                alt="Copy for git clone"
+                                className="copy-icon"
+                            />
+                        </button>
+                    </li>
+                    <li title="cd web-basic-functions">
+                        cd web-basic-functions
+                        <button type="button" onClick={handleCopyToClipboard}>
+                            <img
+                                src={copyIcon}
+                                alt="Copy for folder web-basic-function"
+                                className="copy-icon"
+                            />
+                        </button>
+                    </li>
+                    <li title="yarn install">
+                        yarn install
+                        <button type="button" onClick={handleCopyToClipboard}>
+                            <img
+                                src={copyIcon}
+                                alt="Copy for yarn install"
+                                className="copy-icon"
+                            />
+                        </button>
+                    </li>
+                    <li title="yarn dev">
+                        yarn dev - to run application
+                        <button type="button" onClick={handleCopyToClipboard}>
+                            <img
+                                src={copyIcon}
+                                alt="Copy for yarn dev"
+                                className="copy-icon"
+                            />
+                        </button>
+                    </li>
+                    <li title="yarn build">
+                        yarn build - to generate the package in the dist folder
+                        <button type="button" onClick={handleCopyToClipboard}>
+                            <img
+                                src={copyIcon}
+                                alt="Copy for yarn build"
+                                className="copy-icon"
+                            />
+                        </button>
+                    </li>
+                    <li title="np">
+                        np - to publish the package
+                        <button type="button" onClick={handleCopyToClipboard}>
+                            <img
+                                src={copyIcon}
+                                alt="Copy to np"
+                                className="copy-icon"
+                            />
+                        </button>
+                    </li>
+                </ol>
+            </section>
+
+            <hr />
+
+            <section>
+                <h2>Documentation</h2>
+                <p>- under construction</p>
+            </section>
         </section>
-
-        <hr />
-
-        <section>
-            <h2>
-                Cloning the Repository With Git and Node.js installed on your
-                machine, choose location and clone repository, use this
-                commands:
-            </h2>
-            <ol>
-                <li>
-                    git clone
-                    https://github.com/Drete457/web-basic-functions.git
-                </li>
-                <li>cd web-basic-functions</li>
-                <li>yarn install</li>
-                <li>yarn dev - to run application</li>
-                <li>yarn build - to generate the package in the dist folder</li>
-                <li>np - to publish the package</li>
-            </ol>
-        </section>
-
-        <hr />
-
-        <section>
-            <h2>Documentation</h2>
-            <p>- under construction</p>
-        </section>
-    </section>
-);
+    );
+};
 
 export default MainPage;
